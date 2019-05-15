8bd553b95907775439968256d2f1b80ba99bbe41
alcomen <alcomen@gmail.com>
2019-05-15 15:06:06 -0300

    Sistema de Rolos V2 com WiFi

---
+++ Sistema_de_Rolos.ino
@@ -0,0 +1,333 @@
+#include <CheapStepper.h> // 
+#include <ESP8266WiFi.h> // Importa a Biblioteca ESP8266WiFi
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClient
+
+#define wifi
+
+// next, declare the stepper
+// and connect pins 8,9,10,11 to IN1,IN2,IN3,IN4 on ULN2003 board
+
+#define TopicMotor      "rolo/motor"     //tópico MQTT de escuta
+#define TopicMotorFlag  "rolo/flag"
+#define TopicSpeed      "rolo/speed"
+#define TopicTemp       "rolo/temp"
+#define TopicWiFi       "rolo/wifi"
+
+#define ID_MQTT  "Rolo"     //id mqtt (para identificação de sessão)
+
+CheapStepper stepper(D5,D6,D7,D8);
+
+char speed_stepper = 20;
+const int stepsPerRevolution = 200;
+
+char msg[5];
+
+int speed_motor = 20;
+
+ // let's also create a boolean variable to save the direction of our rotation
+ // and a timer variable to keep track of move times
+
+bool moveClockwise = true;
+unsigned long moveStartTime = 0; // this will save the time (millis()) when we started each new move
+
+// WIFI
+//const char* SSID = "UnivapWifi"; // SSID / nome da rede WI-FI que deseja se conectar
+//const char* PASSWORD = "universidade"; // Senha da rede WI-FI que deseja se conectar
+
+//const char* SSID = "IPDWifi"; // SSID / nome da rede WI-FI que deseja se conectar
+//const char* PASSWORD = "pesquisa"; // Senha da rede WI-FI que deseja se conectar
+
+const char* SSID = "HOME_DSK"; // SSID / nome da rede WI-FI que deseja se conectar
+const char* PASSWORD = "probesserv"; // Senha da rede WI-FI que deseja se conectar
+
+// MQTT
+//const char* BROKER_MQTT = "iot.eclipse.org"; //URL do broker MQTT que se deseja utilizar
+//const char* BROKER_MQTT = "broker.hivemq.com"; //URL do broker MQTT que se deseja utilizar
+//const char* BROKER_MQTT = "172.17.76.197"; //URL do broker MQTT que se deseja utilizar
+const char* BROKER_MQTT = "192.168.43.3"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1882; // Porta do Broker MQTT
+
+//Variáveis e objetos globais
+WiFiClient espClient; // Cria o objeto espClient
+PubSubClient MQTT(espClient); // Instancia o Cliente MQTT passando o objeto espClient
+
+char EstadoSaida = '0';  //variável que armazena o estado atual da saída
+int flagOut = 0;
+unsigned int passo = 0;
+
+void setup() {
+
+    //inicializações:
+    delay(2000);
+    InitOutput();
+    initSerial();
+#ifdef wifi
+    initWiFi();
+    initMQTT();
+#endif
+
+  // let's run the stepper at 12rpm (if using 5V power) - the default is ~16 rpm
+
+  stepper.setRpm(speed_stepper);
+
+  // let's print out the RPM to make sure the setting worked
+ 
+  Serial.begin(115200);
+  Serial.print("stepper RPM: "); Serial.print(stepper.getRpm());
+  Serial.println();
+
+  //stepper.setSpeed(60);
+}
+
+void loop() {
+
+    int stepsLeft = stepper.getStepsLeft();
+#ifdef wifi
+    //garante funcionamento das conexões WiFi e ao broker MQTT
+    VerificaConexoesWiFIEMQTT();
+#endif 
+    //envia o status de todos os outputs para o Broker no protocolo esperado
+    //EnviaEstadoOutputMQTT();
+
+  // we need to call run() during loop()
+  // in order to keep the stepper moving
+  // if we are using non-blocking moves
+
+    if(flagOut)
+    {
+      stepper.moveTo (moveClockwise, passo);
+      //stepper.newMoveDegrees (moveClockwise, 180);
+      // now we've moved 4096 steps
+  
+      // let's wait one second
+
+      passo+=speed_motor;
+      Serial.print("passo: ");
+      Serial.println(passo);
+      Serial.print("speed motor: ");
+      Serial.println(speed_motor);
+      
+      if(passo>4096) passo = 0;
+      //Serial.println(stepper.getStep());
+  
+      delay(10);
+      
+#ifdef wifi
+          MQTT.publish(TopicMotorFlag, "L");
+#endif   
+    }else
+    {
+#ifdef wifi
+      MQTT.publish(TopicMotorFlag, "D");
+#endif
+      //stepper.stop();
+    }
+
+  ////////////////////////////////
+  // now the stepper is moving, //
+  // let's do some other stuff! //
+  ////////////////////////////////
+
+  // let's check how many steps are left in the current move:
+ 
+ 
+
+  // if the current move is done...
+#ifdef wifi
+    //keep-alive da comunicação com broker MQTT
+    MQTT.loop();
+#endif
+}
+
+//Função: inicializa comunicação serial com baudrate 115200 (para fins de monitorar no terminal serial
+//        o que está acontecendo.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initSerial()
+{
+    Serial.begin(115200);
+}
+ 
+//Função: inicializa e conecta-se na rede WI-FI desejada
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initWiFi()
+{
+    delay(10);
+    Serial.println("------Conexao WI-FI------");
+    Serial.print("Conectando-se na rede: ");
+    Serial.println(SSID);
+    Serial.println("Aguarde");
+    
+    reconectWiFi();
+}
+ 
+//Função: inicializa parâmetros de conexão MQTT(endereço do
+//        broker, porta e seta função de callback)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initMQTT()
+{
+    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+    MQTT.setCallback(mqtt_callback);            //atribui função de callback (função chamada quando qualquer informação de um dos tópicos subescritos chega)
+}
+ 
+//Função: função de callback
+//        esta função é chamada toda vez que uma informação de
+//        um dos tópicos subescritos chega)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void mqtt_callback(char* topic, byte* payload, unsigned int length)
+{
+    String msg;
+    char msg_speed[5];
+ 
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++)
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+  
+    //toma ação dependendo da string recebida:
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+    if (msg.equals("L"))
+    {
+        digitalWrite(D0, HIGH);
+        EstadoSaida = '1';
+        flagOut = 1;
+        Serial.println("MOTOR ON");
+    }
+ 
+    //verifica se deve colocar nivel alto de tensão na saída D0:
+    if (msg.equals("D"))
+    {
+        digitalWrite(D0, LOW);
+        EstadoSaida = '0';
+        flagOut = 0;
+        Serial.println("MOTOR OFF");
+    }
+
+    if (msg.equals("A"))
+    {
+        speed_motor+=5;
+        if(speed_motor > 180) speed_motor = 180;
+        Serial.println("SPEED MOTOR UP");
+        Serial.println(speed_motor);
+    }
+
+    if (msg.equals("B"))
+    {
+        speed_motor-=5;
+        if(speed_motor < 0) speed_motor = 0;
+        Serial.println("SPEED MOTOR DOWN");
+        Serial.println(speed_motor);
+    }
+    sprintf (msg_speed, "%d", speed_motor);
+    MQTT.publish(TopicSpeed, msg_speed);
+    
+}
+ 
+//Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
+//        em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconnectMQTT()
+{
+    while (!MQTT.connected())
+    {
+        Serial.print("* Tentando se conectar ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT))
+        {
+            Serial.println("Conectado com sucesso ao broker MQTT!");
+            MQTT.subscribe(TopicMotor);
+            MQTT.publish(TopicWiFi, "1");
+        }
+        else
+        {
+            Serial.println("Falha ao reconectar no broker.");
+            Serial.println("Havera nova tentatica de conexao em 2s");
+            MQTT.publish(TopicWiFi, "0");
+            delay(2000);
+        }
+        Serial.println();
+        Serial.print("Conectado com sucesso na rede ");
+        Serial.println(SSID);
+        Serial.println("IP obtido: ");
+        Serial.println(WiFi.localIP());
+    }
+    MQTT.publish(TopicWiFi, "1");
+}
+ 
+//Função: reconecta-se ao WiFi
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconectWiFi()
+{
+    //se já está conectado a rede WI-FI, nada é feito.
+    //Caso contrário, são efetuadas tentativas de conexão
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+    Serial.println("Conectando...");    
+    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+    
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
+  
+    Serial.println();
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(SSID);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+}
+ 
+//Função: verifica o estado das conexões WiFI e ao broker MQTT.
+//        Em caso de desconexão (qualquer uma das duas), a conexão
+//        é refeita.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void VerificaConexoesWiFIEMQTT(void)
+{
+    if (!MQTT.connected()){
+        reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+        MQTT.publish(TopicWiFi, "0");
+
+    }else
+        MQTT.publish(TopicWiFi, "1");
+    
+     reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+ 
+//Função: envia ao Broker o estado atual do output
+//Parâmetros: nenhum
+//Retorno: nenhum
+void EnviaEstadoOutputMQTT(void)
+{
+    if (EstadoSaida == '0')
+      MQTT.publish(TopicMotorFlag, "D");
+ 
+    if (EstadoSaida == '1')
+      MQTT.publish(TopicMotorFlag, "L");
+ 
+    Serial.println("- Estado da saida D0 enviado ao broker!");
+    delay(1000);
+}
+ 
+//Função: inicializa o output em nível lógico baixo
+//Parâmetros: nenhum
+//Retorno: nenhum
+void InitOutput(void)
+{
+    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
+    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
+    pinMode(D0, OUTPUT);
+    digitalWrite(D0, HIGH);         
+}
+
+
